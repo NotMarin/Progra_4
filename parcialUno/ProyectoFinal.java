7f2950f5e9dac026c828191a144e62840f359491
Santiago Marin Henao <santimarinh04@gmail.com>
2024-03-22 08:20:50 -0500

    Update ProyectoFinal.java

--- parcialUno/ProyectoFinal.java
+++ parcialUno/ProyectoFinal.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class ProyectoFinal {
     public static Scanner leerDatoTeclado = new Scanner(System.in);
-    public static final int TIPO_DOCUMENTO_IDENTIFICACION = 0;
-    public static final int DOCUMENTO_IDENTIFICACION = 1;
-    public static final int NOMBRES = 2;
-    public static final int CORREO_ELECTRONICO = 6;
-    public static final int CONTRASENA = 8;
+    public static final byte TIPO_DOCUMENTO_IDENTIFICACION = 0;
+    public static final byte DOCUMENTO_IDENTIFICACION = 1;
+    public static final byte NOMBRES = 2;
+    public static final byte APELLIDOS = 3;
+    public static final byte CORREO_ELECTRONICO = 4;
+    public static final byte DIRECCION_DE_RESIDENCIA = 5;
+    public static final byte CIUDAD_DE_RESIDENCIA = 6;
+    public static final byte TELEFONO_DE_CONTACTO = 7;
+    public static final byte CONTRASENA = 8;
+    public static final byte CONFIRMAR_CONTRASENA = 9;
     
     public static List<String[]> usuarios = new ArrayList<>();
 
@@ -68,45 +73,55 @@ public class ProyectoFinal {
   
         System.out.print("Teléfono de contacto: ");
         String telefonoContacto = leerDatoTeclado.nextLine();
-  
-        System.out.print("Contraseña: ");
-        String contrasena = leerDatoTeclado.nextLine();
-  
-        System.out.print("Confirmar contraseña: ");
-        String confirmarContrasena = leerDatoTeclado.nextLine();
+
+        String contrasena = "1";
+        String confirmarContrasena = "0";
+
+        while (!contrasena.equals(confirmarContrasena)) {
+            System.out.print("Contraseña: ");
+            contrasena = leerDatoTeclado.nextLine();
+        
+            System.out.print("Confirmar contraseña: ");
+            confirmarContrasena = leerDatoTeclado.nextLine();
+            
+            if (!contrasena.equals(confirmarContrasena)) {
+                System.out.print("Las contraseñas no coinciden\n");
+            }
+        }
   
         registrarUsuario(tipoIdentificacion, documentoIdentificacion, nombres, apellidos, correoElectronico, direccionResidencia, ciudadResidencia, telefonoContacto, contrasena, confirmarContrasena);
     }
   
     public static void registrarUsuario(String tipoDocumentoIdentificacion, String documentoIdentificacion, String nombres, String apellidos, String correoElectronico, String direccionResidencia, String ciudadResidencia, String telefonoContacto, String contrasena, String confirmarContrasena) {
-        if (!contrasena.equals(confirmarContrasena)) {
-            System.out.println("Las contraseñas no coinciden. Intente nuevamente.");
-            return;
-        }
   
         String[] usuario = new String[10];
         usuario[TIPO_DOCUMENTO_IDENTIFICACION] = tipoDocumentoIdentificacion;
         usuario[DOCUMENTO_IDENTIFICACION] = documentoIdentificacion;
         usuario[NOMBRES] = nombres;
+        usuario[APELLIDOS] = apellidos;
         usuario[CORREO_ELECTRONICO] = correoElectronico;
+        usuario[DIRECCION_DE_RESIDENCIA] = direccionResidencia;
+        usuario[CIUDAD_DE_RESIDENCIA] = ciudadResidencia;
+        usuario[TELEFONO_DE_CONTACTO] = telefonoContacto;
         usuario[CONTRASENA] = contrasena;
+        usuario[CONFIRMAR_CONTRASENA] = confirmarContrasena;
   
         usuarios.add(usuario);
-        System.out.println("Usuario registrado correctamente.");
+        System.out.println("Usuario registrado correctamente.\n");
     }
   
 
     public static void iniciarSesion() {
-        leerDatoTeclado.nextLine(); // Consumir el carácter de nueva línea residual
+        leerDatoTeclado.nextLine();
         System.out.print("Ingrese su correo electrónico: ");
         String correoElectronico = leerDatoTeclado.nextLine();
         System.out.print("Ingrese su contraseña: ");
         String contrasena = leerDatoTeclado.nextLine();
   
         if (validarCredenciales(correoElectronico, contrasena)) {
-            System.out.println("Usuario logueado correctamente.");
+            System.out.println("Usuario logueado correctamente.\n");
         } else {
-            System.out.println("Usuario incorrecto, intente una vez más.");
+            System.out.println("Usuario incorrecto, intente una vez más.\n");
         }
     }
 
